0c67fff64c6a2677cdc955d9a522a4d21b9aee6a
Gellert Peter-Siskovits <geraldlionb@gmail.com>
2019-01-14 14:28:31 +0000

    [dev] initial architecture

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,10 +1,15 @@
+/*global history */
 sap.ui.define([
-	"sap/com/xsOdata_tutorial/BaseController"
-], function (BaseController) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller, History) {
 	"use strict";
 
-	return BaseController.extend("sap.com.xsOdata_tutorial.controller.App", {
+	return Controller.extend("sap.com.xsOdata_tutorial.controller.BaseController", {
+
 		onInit: function () {
+			console.error("BaseController");
+
 			var oModel = this.getOwnerComponent().getModel();
 			var oModel2 = this.getOwnerComponent().getModel("AppDataModel");
 			oModel.read("/CUSTOMERS", {
@@ -15,9 +20,15 @@ sap.ui.define([
 					console.error("oError", oError)
 				}
 			});
-			
+
 			console.error("oModel", oModel);
 			console.error("oModel", oModel2);
+		},
+
+		getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
 		}
+
 	});
+
 });
---
+++ webapp/controller/Master.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/com/xsOdata_tutorial/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.com.xsOdata_tutorial.controller.Master", {
+
+	
+		
+
+		
+
+	});
+
+});
